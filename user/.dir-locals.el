3b52570e69e2f6244bbd0af75f705767c7070cf9
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2014-08-15 16:16:17 -0400

    automate coding conventions for emacs users

---
+++ user/.dir-locals.el
@@ -0,0 +1,10 @@
+;; emacs local configuration settings for tcpcrypt source
+;; surmised by dkg on 2014-08-15
+
+((c-mode
+  (indent-tabs-mode . t)
+  (tab-width . 8)
+  (c-file-style . "linux"))
+ (nil
+  (fill-column . 70))
+ )

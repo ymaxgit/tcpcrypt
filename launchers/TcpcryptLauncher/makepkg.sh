2f0e9ee7e7c6f65c414f7f2a32342c4306ca568a
Andrea Bittau <bittau@cs.stanford.edu>
2016-03-05 08:05:01 -0800

    osx code signing

--- launchers/TcpcryptLauncher/makepkg.sh
+++ launchers/TcpcryptLauncher/makepkg.sh
@@ -4,3 +4,7 @@ DIR=$1
 
 pkgbuild --root $DIR --identifier org.tcpcrypt.TcpcryptLauncher \
 	--version 0.4 --install-location /Applications  tcpcrypt.pkg
+
+productsign --sign 'Developer ID Installer' tcpcrypt.pkg tcpcrypt-signed.pkg
+spctl --asses --type install tcpcrypt-signed.pkg
+mv tcpcrypt-signed.pkg tcpcrypt.pkg

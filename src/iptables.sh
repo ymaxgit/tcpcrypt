73b5942ea79d3f837a8232d0fa5a8a5a22e9a8b5
Daniel B. Giffin <daniel@beech-grove.net>
2016-03-25 15:59:25 -0700

    Improve readability of iptables.sh; no changes to its effect.

--- src/iptables.sh
+++ src/iptables.sh
@@ -1,37 +1,75 @@
 #!/bin/sh
 
 PR=80
+from_enabled_port="--sport $PR"
+to_enabled_port="--dport $PR"
+
+NFQUEUE="NFQUEUE --queue-num 666"
+RDR="65530"
+DAEMON_UID="tcpcryptd"
+INJECT_TOS="0x22"
+HANDSHAKE_TOS="0x04"
 
 OP="-A"
-
 if [ "$1" = "-f" ] ; then
 	OP="-D"
 fi
 
-iptables $OP INPUT -i lo -p tcp --dport 65530 -m tos --tos 0x22 -j ACCEPT
 
-iptables $OP INPUT -p tcp \! -s 127.0.0.1 --dport 65530 --tcp-flags ALL SYN \
-	-j NFQUEUE --queue-num 666
+filter="iptables -t filter $OP"
 
-iptables $OP INPUT -p tcp --sport $PR --tcp-flags ALL SYN,ACK \
-	-j NFQUEUE --queue-num 666
+# injection from daemon: accept
+$filter INPUT -i lo -p tcp --dport $RDR \
+              -m tos --tos $INJECT_TOS \
+  -j ACCEPT
+
+# SYN redirected to daemon:
+#   queue for daemon to initiate proxy connection with original destination
+$filter INPUT -p tcp \! -s 127.0.0.1 --dport $RDR --tcp-flags ALL SYN \
+  -j $NFQUEUE
+
+# SYN+ACK on proxy connection:
+#   queue for daemon to complete original handshake
+$filter INPUT -p tcp $from_enabled_port --tcp-flags ALL SYN,ACK \
+  -j $NFQUEUE
 
 
-iptables $OP OUTPUT -p tcp --dport $PR -m tos --tos 0x4 \
-	-m owner --uid-owner tcpcryptd -j NFQUEUE --queue-num 666
+# handshake packet of proxy connection from daemon:
+#   queue for daemon to set tcp options via DIVERT_MODIFY
+$filter OUTPUT -p tcp $to_enabled_port \
+               -m tos --tos $HANDSHAKE_TOS \
+               -m owner --uid-owner $DAEMON_UID \
+  -j $NFQUEUE
 
-iptables $OP OUTPUT -p tcp --sport 65530 --tcp-flags ALL SYN,ACK \
-	-j NFQUEUE --queue-num 666
+# SYN+ACK for redirected connection:
+#   queue for daemon to delay handshake until proxy connection succeeds
+$filter OUTPUT -p tcp --sport $RDR --tcp-flags ALL SYN,ACK \
+  -j $NFQUEUE
 
 
-iptables -t nat $OP PREROUTING -p tcp --dport $PR -j REDIRECT --to-port 65530
+nat="iptables -t nat $OP"
+
+# inbound connection for enabled ports:
+#   redirect to daemon for encryption
+# (the nat module handles port-mapping in both directions)
+$nat PREROUTING -p tcp $to_enabled_port \
+  -j REDIRECT --to-port $RDR
 
 
-iptables -t nat $OP OUTPUT -p tcp --dport $PR -m owner --uid-owner tcpcryptd \
-	-j ACCEPT
+# proxy connection from daemon to enabled port: accept
+$nat OUTPUT -p tcp $to_enabled_port \
+            -m owner --uid-owner $DAEMON_UID \
+  -j ACCEPT
 
-iptables -t nat $OP OUTPUT -p tcp \! -o lo --dport $PR \
-	-j REDIRECT --to-port 65530
+# outbound connections to enabled ports on remote hosts:
+#   redirect to daemon for encryption
+# (the nat module handles port-mapping in both directions)
+$nat OUTPUT \! -o lo -p tcp $to_enabled_port \
+  -j REDIRECT --to-port $RDR
 
-###### clean up all ths tos tricks we've been doing
-iptables -t mangle $OP POSTROUTING -m tos --tos 0x04 -j TOS --set-tos 0x00
+
+mangle="iptables -t mangle $OP"
+
+# packets leaving the machine with bookkeeping mark: remove mark
+$mangle POSTROUTING -m tos --tos $HANDSHAKE_TOS \
+  -j TOS --set-tos 0x00

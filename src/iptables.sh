fa8b4cc173ab6d1130bdf48953aaa2cc28b106ce
Daniel B. Giffin <daniel@beech-grove.net>
2016-03-28 00:44:35 -0700

    iptables: slight wording changes to comments

--- src/iptables.sh
+++ src/iptables.sh
@@ -18,58 +18,62 @@ fi
 
 filter="iptables -t filter $OP"
 
-# injection from daemon: accept
+# Injection from daemon: Accept
 $filter INPUT -i lo -p tcp --dport $RDR \
               -m tos --tos $INJECT_TOS \
   -j ACCEPT
 
 # SYN redirected to daemon:
-#   queue for daemon to initiate proxy connection with original destination
+#   Queue for daemon to initiate proxy connection with original destination
 $filter INPUT -p tcp \! -s 127.0.0.1 --dport $RDR --tcp-flags ALL SYN \
   -j $NFQUEUE
 
 # SYN+ACK on proxy connection:
-#   queue for daemon to complete original handshake
+#   Queue for daemon to complete original handshake
 $filter INPUT -p tcp $from_enabled_port --tcp-flags ALL SYN,ACK \
   -j $NFQUEUE
 
 
-# handshake packet of proxy connection from daemon:
-#   queue for daemon to set tcp options via DIVERT_MODIFY
+# Handshake packet of proxy connection from daemon:
+#   Queue for daemon to set tcp options via DIVERT_MODIFY
 $filter OUTPUT -p tcp $to_enabled_port \
                -m tos --tos $HANDSHAKE_TOS \
                -m owner --uid-owner $DAEMON_UID \
   -j $NFQUEUE
 
-# SYN+ACK for redirected connection:
-#   queue for daemon to delay handshake until proxy connection succeeds
+# SYN+ACK on redirected connection:
+#   Queue for daemon to delay handshake until proxy connection succeeds
 $filter OUTPUT -p tcp --sport $RDR --tcp-flags ALL SYN,ACK \
   -j $NFQUEUE
 
 
 nat="iptables -t nat $OP"
 
-# inbound connection for enabled ports:
-#   redirect to daemon for encryption
-# (the nat module handles port-mapping in both directions)
+# Inbound connection for enabled ports:
+#   Redirect to daemon (at localhost:$RDR) for encryption
+#
+# (The nat module will now translate addresses in both directions,
+#  for the lifetime of this connection.)
 $nat PREROUTING -p tcp $to_enabled_port \
   -j REDIRECT --to-port $RDR
 
 
-# proxy connection from daemon to enabled port: accept
+# Proxy connection from daemon to enabled port: Accept
 $nat OUTPUT -p tcp $to_enabled_port \
             -m owner --uid-owner $DAEMON_UID \
   -j ACCEPT
 
-# outbound connections to enabled ports on remote hosts:
-#   redirect to daemon for encryption
-# (the nat module handles port-mapping in both directions)
+# Outbound connections to enabled ports on remote hosts:
+#   Redirect to daemon (at localhost port $RDR) for encryption
+#
+# (The nat module will now translate addresses in both directions,
+#  for the lifetime of this connection.)
 $nat OUTPUT \! -o lo -p tcp $to_enabled_port \
   -j REDIRECT --to-port $RDR
 
 
 mangle="iptables -t mangle $OP"
 
-# packets leaving the machine with bookkeeping mark: remove mark
+# Packets leaving the machine with bookkeeping mark: Remove mark
 $mangle POSTROUTING -m tos --tos $HANDSHAKE_TOS \
   -j TOS --set-tos 0x00
